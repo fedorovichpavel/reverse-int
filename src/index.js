e1d8a7494c5ff6a2777fb5b0ab28602c0d568dae
Pavel Fedarovich <fedorovich.pavel@mail.ru>
2020-10-11 16:15:45 +0300

    feat:add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let number = n.toString().split('').reverse().join('');
+    if (number[number.length - 1] == '-') {
+        number = number.substring(0, number.length - 1);
+    }
+    return +number;
 }
